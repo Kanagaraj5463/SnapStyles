b4182bb16e606b33d0538ff760a953d2fccec182
Kanagaraj Aruchamy <kanaguphysics@gmail.com>
2026-08-17 23:42:03 +0530

    Initial Commit

---
+++ SnapStyles/SnapStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapStylesApp.swift
+//  SnapStyles
+//
+//  Created by Kanagaraj on 17/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SnapStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
